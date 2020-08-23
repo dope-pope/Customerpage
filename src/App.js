838816c17899500a2b5812e4001589b52f76ebaa
dope-pope <vikas.kumar8604@gmail.com>
2020-08-23 11:44:09 +0530

    adding the page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Card, Button, CardBody, CardImg, CardTitle, CardText, CardImgOverlay, CardFooter, CardHeader } from 'reactstrap';
 import './App.css';
+import { COSTUMERS } from './data/costumer';
+import Avatar from '@material-ui/core/Avatar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+const App = () => (
+    <div className="costumer ">
+
+        {COSTUMERS.map((costumer) => {
+            return (
+                
+                <div className="costumers ">
+                    <div className="row header  mt-5 ml-3">
+                        <Avatar alt="Remy Sharp" src={costumer.dp} />
+                        <p className="name"> {costumer.name}</p>
+                    </div>
+                    <div className=" img  ">
+                        <img src={costumer.image} alt={costumer.name} />
+                    </div>
+                    <div className="  description ">
+                        <p id="des">{costumer.description}</p>
+                    </div>
+                    <div className=" button">
+                        <button type="button" class="btn btn-info">Accept</button>
+                        <button type="button" class="btn btn-secondary btn2">Reject</button>
+                    </div>
+                    </div>
+                   
+            )
+        })};
+
     </div>
-  );
-}
 
+)
 export default App;
---
+++ src/data/costumer.js
@@ -0,0 +1,40 @@
+export const COSTUMERS =
+    [
+        {
+            id: 0,
+            name: 'Customer!',
+            image: '../../assets/lol.png',
+            category: 'mains',
+            dp: '../../assets/dp.jpg',
+            price: '4.99',
+            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        },
+        {
+            id: 1,
+            name: 'Customer2',
+            image: '../../assets/lol.png',
+            category: 'appetizer',
+            dp: '../../assets/dp.jpg',
+            price: '1.99',
+            description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+
+        },
+        {
+            id: 2,
+            name: 'Customer3',
+            image: '../../assets/lol.png',
+            category: 'appetizer',
+            dp: '../../assets/dp.jpg',
+            price: '1.99',
+            description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+        },
+        {
+            id: 3,
+            name: 'Customer4',
+            image: '../../assets/lol.png',
+            category: 'dessert',
+            dp: '../../assets/dp.jpg',
+            price: '2.99',
+            description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+        }
+    ];
